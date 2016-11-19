f27c370ae07c8763c5bf5af6dd7f5a5dc746a08e
xudequan <827701288@qq.com>
2016-11-19 22:19:15 +0800

    vue1

---
+++ js/index.js
@@ -0,0 +1,123 @@
+//var Parent = Vue.extend({
+//  template:"<div>父集的模版</div>",
+//});
+new Vue({
+	el:"#app",
+	data:{
+		imgsrc:"https://news.ycombinator.com/y18.gif",
+		listData:[{
+			ahref:"https://eugene.kaspersky.com/2016/11/15/finally-our-own-os-oh-yes/",
+			introduce:"Kaspersky OS",
+			remarks:"eugene.kaspersky.com",
+			text1:"409",
+			text2:"itiman",
+			text3:"23 hours |",
+			text4:"270 comments"
+		},{
+			ahref:"http://quoteinvestigator.com/2012/04/28/shorter-letter/",
+			introduce:"If I Had More Time, I Would Have Written a Shorter Letter (2012)",
+			remarks:"quoteinvestigator.com",
+			text1:"116 points by ",
+			text2:"curtis",
+			text3:"1 day ago | ",
+			text4:"comments"
+		},{
+			ahref:"http://dmitry.gr/index.php?r=05.Projects&proj=21.%20KLN89",
+			introduce:"Updating my plane’s very old GPS data in a very modern way",
+			remarks:"dmitry.gr",
+			text1:"179 points by ",
+			text2:"dmitrygr",
+			text3:"20 hours ago | ",
+			text4:" 53 comments"
+		},{
+			ahref:"https://twitter.com/ErrataRob/status/799556482719162368",
+			introduce:"New security camera compromised by worm within minutes of installation",
+			remarks:"twitter.com",
+			text1:"234 points by ",
+			text2:"bpierre",
+			text3:"18 hours ago | ",
+			text4:"78 comments"
+		},{
+			ahref:"https://twitter.com/ErrataRob/status/799556482719162368",
+			introduce:"New security camera compromised by worm within minutes of installation",
+			remarks:"twitter.com",
+			text1:"234 points by ",
+			text2:"bpierre",
+			text3:"18 hours ago | ",
+			text4:"78 comments"
+		},{
+			ahref:"https://twitter.com/ErrataRob/status/799556482719162368",
+			introduce:"New security camera compromised by worm within minutes of installation",
+			remarks:"twitter.com",
+			text1:"234 points by ",
+			text2:"bpierre",
+			text3:"18 hours ago | ",
+			text4:"78 comments"
+		},{
+			ahref:"https://twitter.com/ErrataRob/status/799556482719162368",
+			introduce:"New security camera compromised by worm within minutes of installation",
+			remarks:"twitter.com",
+			text1:"234 points by ",
+			text2:"bpierre",
+			text3:"18 hours ago | ",
+			text4:"78 comments"
+		},{
+			ahref:"https://twitter.com/ErrataRob/status/799556482719162368",
+			introduce:"New security camera compromised by worm within minutes of installation",
+			remarks:"twitter.com",
+			text1:"234 points by ",
+			text2:"bpierre",
+			text3:"18 hours ago | ",
+			text4:"78 comments"
+		},{
+			ahref:"https://twitter.com/ErrataRob/status/799556482719162368",
+			introduce:"New security camera compromised by worm within minutes of installation",
+			remarks:"twitter.com",
+			text1:"234 points by ",
+			text2:"bpierre",
+			text3:"18 hours ago | ",
+			text4:"78 comments"
+		},{
+			ahref:"https://twitter.com/ErrataRob/status/799556482719162368",
+			introduce:"New security camera compromised by worm within minutes of installation",
+			remarks:"twitter.com",
+			text1:"234 points by ",
+			text2:"bpierre",
+			text3:"18 hours ago | ",
+			text4:"78 comments"
+		}]
+	}
+//	components: {
+//  	'parent': Parent
+//	},
+	
+});
+
+
+
+//var Home = Vue.extend({
+//  template: '<div><h1>Home</h1><p>{{msg}}</p></div>',
+//  data: function() {
+//      return {
+//          msg: 'Hello, vue router!'
+//      }
+//  }
+//})
+//var About = Vue.extend({
+//  template: '<div><h1>About</h1><p>This is the tutorial about vue-router.</p></div>'
+//})
+//
+//var router = new VueRouter();
+//router.map({
+//  '/home': { component: Home },
+//  '/about': { component: About }
+//});
+//var App = Vue.extend({
+//	template: '<div><h1>Home</h1><p>{{msg}}</p></div>',
+//  data: function() {
+//      return {
+//          msg: 'Hello, vue router!'
+//      }
+//  }
+//});
+//router.start(App, '#app')
